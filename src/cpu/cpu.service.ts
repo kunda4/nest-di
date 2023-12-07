4834d25cb22d1fc76d1888d5669d5759f69104d1
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-07 15:49:43 +0200

    connect disk to power module

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,9 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+  computePower(a: number, b: number) {
+    console.log('Draweing 10 watts of power from PowerServices');
+    this.powerService.SupplyPower(10);
+    return a + b;
+  }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+  get() {
+    console.log('Drawing some amout of power in power supply');
+    this.powerService.SupplyPower(20);
+    return 'data';
+  }
+}
